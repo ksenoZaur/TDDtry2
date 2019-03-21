c85b70fc3098db7e56c426c8bd4ff0e9862fe3e4
davemustaine <ksenofontov97@mail.ru>
2019-03-21 12:31:32 +0700

    Генерация кода Adapter на C#

--- src/sample/Generator.java
+++ src/sample/Generator.java
@@ -14,14 +14,23 @@ public class Generator {
 
     public String generate(int i, int i1) {
 
-        return "interface RoundPeg {private int radius;public RoundPeg( int radius );public int " +
-                "getRadius();}class SquarePeg{private int width;public SquarePeg( int width ){this.width " +
-                "= width;}public int getWidth(){return this.width;}}class SquarePegAdapter implements " +
-                "RoundPeg{private SquarePeg peg;public SquarePegAdapter( SquarePeg peg ) {this.peg = peg;}" +
-                "@Override public int getRadius() {return (int)(peg.getWidth() * Math.sqrt(2)/2);}}public " +
-                "class RoundHole {private int radius;public int getRadius() {return this.radius;}public " +
-                "RoundHole( int radius ) {this.redius = radius;}public bool fits( RoundPeg peg ){if( " +
-                "peg.getRadius() <= this.radius )return true;elsereturn false;}}";
+        if( i1 == 0 )
+            return "interface RoundPeg {private int radius;public RoundPeg( int radius );public int " +
+                    "getRadius();}class SquarePeg{private int width;public SquarePeg( int width ){this.width " +
+                    "= width;}public int getWidth(){return this.width;}}class SquarePegAdapter implements " +
+                    "RoundPeg{private SquarePeg peg;public SquarePegAdapter( SquarePeg peg ) {this.peg = peg;}" +
+                    "@Override public int getRadius() {return (int)(peg.getWidth() * Math.sqrt(2)/2);}}public " +
+                    "class RoundHole {private int radius;public int getRadius() {return this.radius;}public " +
+                    "RoundHole( int radius ) {this.redius = radius;}public bool fits( RoundPeg peg ){if( " +
+                    "peg.getRadius() <= this.radius )return true;elsereturn false;}}";
+        else
+            return "\uFEFFusing System;  namespace Adapter {    class MainApp  {    static void Main()    {" +
+                    "      RoundPeg roundPeg = new SquarePegAdapter();      roundPeg.getRadius();      " +
+                    "RoundHole hole = new RoundHole(radius);hod.fits(roundPeg);    }  }   class RoundPeg  {    " +
+                    "int radius;    public virtual int getRadius()    {      return radius;    }  }   class " +
+                    "SquarePegAdapter : RoundPeg  {    private SquarePeg adaptee = new SquarePeg();     public " +
+                    "override int getRadius()    {      adaptee.getWidth();    }  }   class SquarePeg  " +
+                    "{int width;    public int getWidth()    {      return width;    }  } }";
 
     }
 }
