3bc1f0bcb6ae08cca3f834b1a6b0435636450352
davemustaine <ksenofontov97@mail.ru>
2019-03-21 12:17:01 +0700

    Добавлен метод generate(int, int)

--- src/sample/Generator.java
+++ src/sample/Generator.java
@@ -11,4 +11,8 @@ public class Generator {
     public ArrayList<String> getLanguages() {
         return  new ArrayList<String>() {{ add("Java"); add("C#");  add("JavaScript");}};
     }
+
+    public void generate(int i, int i1) {
+
+    }
 }
