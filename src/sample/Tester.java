0d5797f3f40a8c4973e32ae7f584394de9f45a67
davemustaine <ksenofontov97@mail.ru>
2019-03-21 10:59:18 +0700

    Тест на создание обьекта Generator

---
+++ src/sample/Tester.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Tester {
+
+    @Test
+    public void testCreateObject(){
+
+        Generator object = new Generator();
+
+    }
+
+
+
+}
