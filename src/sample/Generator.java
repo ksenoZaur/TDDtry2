ac6d5e58de5c41a4a174bf8b98ea077a7f2a9021
davemustaine <ksenofontov97@mail.ru>
2019-03-21 12:30:00 +0700

    Генерация кода Adapter на Java

--- src/sample/Generator.java
+++ src/sample/Generator.java
@@ -14,6 +14,14 @@ public class Generator {
 
     public String generate(int i, int i1) {
 
-        return null;
+        return "interface RoundPeg {private int radius;public RoundPeg( int radius );public int " +
+                "getRadius();}class SquarePeg{private int width;public SquarePeg( int width ){this.width " +
+                "= width;}public int getWidth(){return this.width;}}class SquarePegAdapter implements " +
+                "RoundPeg{private SquarePeg peg;public SquarePegAdapter( SquarePeg peg ) {this.peg = peg;}" +
+                "@Override public int getRadius() {return (int)(peg.getWidth() * Math.sqrt(2)/2);}}public " +
+                "class RoundHole {private int radius;public int getRadius() {return this.radius;}public " +
+                "RoundHole( int radius ) {this.redius = radius;}public bool fits( RoundPeg peg ){if( " +
+                "peg.getRadius() <= this.radius )return true;elsereturn false;}}";
+
     }
 }
